18382d3b53bb5f08244770a25f403476c93a7e25
Ore <orepeace@gmail.com>
2025-09-11 21:37:38 +0100

    chore: remove generated files from dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { EventStoreClient, Event, EventToSave, SaveEventsRequest, GetEventsRequest, SubscribeRequest, Position, Tag, Criterion, Query, EventStoreClientOptions } from './client';
-import { EventStoreClient } from './client';
-export default EventStoreClient;
-//# sourceMappingURL=index.d.ts.map
--- dist/src/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { EventStoreClient, Event, EventToSave, SaveEventsRequest, GetEventsRequest, SubscribeRequest, Position, Tag, Criterion, Query, RetryPolicy, EventStoreClientOptions, Logger } from './client';
-import { EventStoreClient } from './client';
-export default EventStoreClient;
-//# sourceMappingURL=index.d.ts.map
